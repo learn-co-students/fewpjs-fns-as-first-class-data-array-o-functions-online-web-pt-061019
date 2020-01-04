9b3d1fcf713378b8ae52a304c51dff22ae99469a
scrabill <shannon.crabill@gmail.com>
2020-01-03 19:55:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 function wakeDog(a,b) {
   console.log(`Wake ${a} the ${b}`);
+  return `Wake ${a} the ${b}`;
 }
-function leashDog() {
-
+function leashDog(a,b) {
+  console.log(`Leash ${a} the ${b}`);
+  return `Leash ${a} the ${b}`;
 }
-function walkToPark() {
-
+function walkToPark(a,b) {
+  console.log(`Walk to the park with ${a} the ${b}`);
+  return `Walk to the park with ${a} the ${b}`;
 }
-function throwFrisbee() {
-
+function throwFrisbee(a,b) {
+  console.log(`Throw the frisbee for ${a} the ${b}`);
+  return `Throw the frisbee for ${a} the ${b}`;
 }
-function walkHome() {
-
+function walkHome(a,b) {
+  console.log(`Walk home with ${a} the ${b}`);
+  return `Walk home with ${a} the ${b}`;
 }
-function unleashDog() {
-
+function unleashDog(a,b) {
+  console.log(`Unleash ${a} the ${b}`);
+  return `Unleash ${a} the ${b}`;
 }
 
-const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
+const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog];
+let output = [];
 
 function exerciseDog(dogName, dogBreed) {
 
@@ -27,11 +34,14 @@ function exerciseDog(dogName, dogBreed) {
 // capture the result of each function's call
 // return an Array of all those functions' return values
 
-const array = []
+let index = 0
 
-for (var i = 0; i < routine.length; i++) {
-  // array.push(routine[i](dogName,dogBreed));
-  routine[i](dogName, dogBreed);
-}
-
+  for (index = 0; index < routine.length; index++) {
+      // console.log(array[index]);
+      var routineToRun = routine[index];
+      var outputToSave = routineToRun(dogName, dogBreed);
+      output.push(outputToSave);
+  }
+  // console.log(output.length); // 6
+  return output;
 }
